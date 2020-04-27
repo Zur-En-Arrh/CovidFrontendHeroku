9d81c6eaeefc6086be92b4428b22a75ca2b25506
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-04-26 23:42:29 -0300

    Corrigido

--- js/funcoes.js
+++ js/funcoes.js
@@ -6,6 +6,24 @@ let service = null
 let markers = []
 let filtro = null
 let busca = []
+
+const JASON = `{
+    "Elite":{"Value":4},
+    "K":{"Value":10},
+    "M":{"Value":8},
+    "Mutant":{"Value":2},
+    "N":{"Value":20},
+    "P":{"Value":20},
+    "S":{"Value":"1"},
+    "Type":{"Value":true},
+    "City":{"Value":["RS-4","MG-2","RN-3","RS-4","RJ-2","MG-0","RJ-0","SC-0","AC-1","RJ-3","RJ-2","RS-2","SC-2","RJ-6","RN-3","RN-6","SC-4","SC-2","AM-2","AM-3","AP-4","AP-3","RJ-1","RO-4","MT-2","MT-4","RS-4","GO-4","RS-0"]},
+    "Cover":{"Value":[5400,3580,2351,3223,4123,3212,2512,5132,8078,3124,4151,5131,5643,5131,5132,9787,5779,5808,507,7507,3122,3125,6432,1245,6421,1425,1315,6432,5122]},
+    "Cost":{"Value":[321321,753654,523334,321412,321517,635523,516821,215168,512356,132186,972198,351285,865565,841798,135652,571971,312979,312756,319267,946732,321512,521321,211514,312312,315164,875435,735223,642189,879768]}
+}`
+
+
+
+
 function initMap() {
 
     const center = {lat: -23.3256 , lng: -46.3820}
@@ -174,19 +192,55 @@ async function ajax() {
     const resposta = await axios.post(urlheroku, {lugares: busca})
     console.log(resposta)
     var inputJson = {}
-    inputJson.N = {Value: document.getElementById("txtN").value}
-    inputJson.M = {Value: document.getElementById("txtM").value}
-    inputJson.P = {Value: document.getElementById("txtP").value}
-    inputJson.Elite = {Value: document.getElementById("txtElite").value}
-    inputJson.Mutant = {Value: document.getElementById("txtMutant").value}
-    inputJson.K = {Value: document.getElementById("txtK").value}
+    inputJson.N = {Value: parseInt(document.getElementById("txtN").value)}
+    inputJson.M = {Value: parseInt(document.getElementById("txtM").value)}
+    inputJson.P = {Value: parseInt(document.getElementById("txtP").value)}
+    inputJson.Elite = {Value: parseInt(document.getElementById("txtElite").value)}
+    inputJson.Mutant = {Value: parseInt(document.getElementById("txtMutant").value)}
+    inputJson.K = {Value: parseInt(document.getElementById("txtK").value)}
     inputJson.S = {Value: document.getElementById("txtS").value}
     inputJson.Type = {Value: document.getElementById("txtType").checked}
-    inputJson.Cover = resposta.data.cover
-    inputJson.Cost = resposta.data.cost
-    inputJson.City = resposta.data.city
-    console.log(JSON.stringify(inputJson))
-    var URL = "https://covid-19-flask-api.herokuapp.com/brkga";
-    const segundaResposta = await axios.post(URL, JSON.stringify(inputJson), {headers: {'Content-Type': 'application/json'}}) 
-    console.log(segundaResposta)
+    inputJson.Cover = {Value: resposta.data.cover.value}
+    inputJson.Cost = {Value: resposta.data.cost.value}
+    inputJson.City = {Value: resposta.data.city.value}
+    console.log(montarJSON(inputJson))
+    /*var URL = "https://covid-19-flask-api.herokuapp.com/brkga";
+    const segundaResposta = await axios.post(URL, JSON, {headers: {'Content-Type': 'application/json'}}) 
+    console.log(segundaResposta)*/
+}
+
+function montarJSON(obj) {
+    let str = `
+    {
+        "Elite":{"Value":${obj.Elite}},
+        "K":{"Value":${obj.K}},
+        "M":{"Value":${obj.M}},
+        "Mutant":{"Value":${obj.Mutant}},
+        "N":{"Value":${obj.N}},
+        "P":{"Value":${obj.P}},
+        "S":{"Value":"${obj.S}"},
+        "Type":{"Value":${obj.Type}},
+    `
+
+    str += `City: {Value: [`
+    let reduce = obj.city.value.reduce((acumulador, atual) => `${acumulador}"${atual}",`,``)
+    let stringFinal = reduce.substring(0,(reduce.length - 1));
+    str += stringFinal
+    str += `]},`
+
+
+    str += `Cost: {Value: [`
+    let reduce = obj.cost.value.reduce((acumulador, atual) => `${acumulador}${atual},`,``)
+    let stringFinal = reduce.substring(0,(reduce.length - 1));
+    str += stringFinal
+    str += `]},`
+
+    str += `Cover: {Value: [`
+    let reduce = obj.cost.value.reduce((acumulador, atual) => `${acumulador}${atual},`,``)
+    let stringFinal = reduce.substring(0,(reduce.length - 1));
+    str += stringFinal
+    str += `]}`
+
+    str += `}`
+    return str
 }
