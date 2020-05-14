4ac64c628a42eeb99220637550505cb8464d8590
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-13 23:22:05 -0300

    *SPA*

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = express()
 //Permite usar o caminho para ver os arquivos na pasta
 app.use(express.static('.'))
 
-app.listen(8080, () => console.log('Executando...'))
+//process.env.PORT || 8080
+app.listen(process.env.PORT || 8080, () => console.log('Executando...'))
