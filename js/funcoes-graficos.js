2cd32caedfeb83c2b9d0e500f67a8f88cb34368b
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-17 22:33:04 -0300

    *SPA-Graficos*

--- js/funcoes-graficos.js
+++ js/funcoes-graficos.js
@@ -2,22 +2,38 @@ const FLASK_API_BASE_URL = 'https://covid-19-flask-api.herokuapp.com/'
 const JS_API_BASE_URL = 'http://localhost:3000/'
 
 function fillComboBox() {
+    const formGroup = document.createElement('div')
+    formGroup.setAttribute('class', 'form-group')
+    const label = document.createElement('label')
+    label.setAttribute('for', 'dados')
     const combo = document.querySelector('select#csv')
-    const elemento = document.querySelector('select#dados')
+    const elemento = document.createElement('select')
+    elemento.setAttribute('id', 'dados')
+    elemento.setAttribute('class', 'form-control')
     const item = combo.options[combo.selectedIndex].value
-    axios(JS_API_BASE_URL+`data/${item}`)
-        .then(resposta => {
-            elemento.innerHTML = ''
-            resposta.data.forEach(obj => {
-                const child = document.createElement('option')
-                child.innerHTML = obj
-                elemento.appendChild(child)
+    if(item == 'Cities')
+        label.innerText = 'Cidades'
+    else
+        label.innerText = 'Estados'
+    formGroup.appendChild(label)
+    if(item === 'Cities' || item === 'States') {
+        axios(JS_API_BASE_URL+`data/${item}`)
+            .then(resposta => {
+                elemento.innerHTML = ''
+                resposta.data.forEach(obj => {
+                    const child = document.createElement('option')
+                    child.innerHTML = obj
+                    elemento.appendChild(child)
+                })
+                formGroup.appendChild(elemento)
+                document.querySelector('div#select-group').appendChild(formGroup)
             })
-        })
-        .catch(err => console.log(err))
+            .catch(err => console.log(err))
+    }
 }
 
 function createCheckBoxes() {
+
     const combo = document.querySelector('select#csv')
     const elemento = document.querySelector('div#chkDados')
     const item = combo.options[combo.selectedIndex].value
@@ -25,8 +41,10 @@ function createCheckBoxes() {
         .then(resposta => {
             elemento.innerHTML = ''
             resposta.data.forEach(obj => {
-                const child = `<label for="id${obj}">${obj}</label> 
-                <input type="checkbox" id="id${obj}" value="${obj}" name="chk${item}"/> <br/>`
+                const child = `<div class="form-check"> 
+                <input type="checkbox" class="form-check-input" id="id${obj}" value="${obj}" name="chk${item}"/> 
+                <label class="form-check-label" for="id${obj}">${obj}</label>
+                </div>`
                 elemento.innerHTML += child
             })
         })
@@ -34,20 +52,83 @@ function createCheckBoxes() {
 }
 
 
-async function ajaxGraficoDinamico(url,body) {
-    console.log(JSON.stringify(body))
+async function plotsAjax(url,body, tipo) {
+    //console.log(JSON.stringify(body))
     try {
-        const resposta = await axios.post(url, body)
+        const resposta = await axios.post(url, body, {headers: {'Content-Type': 'application/json'}})
         const graficoURL = resposta.data
-        const link = document.createElement('a')
-        link.setAttribute('href', graficoURL)
-        link.innerText = 'Ver Gráfico'
-        document.body.appendChild(link)
+        const linha = document.createElement('tr')
+        const colunaView = document.createElement('td')
+        const colunaNome = document.createElement('td')
+        
+        const linkView = document.createElement('a')
+        linkView.setAttribute('href', graficoURL)
+        linkView.setAttribute('target', '_blank')
+        linkView.innerText = 'Ver'
+
+        colunaNome.innerText = tipo
+        colunaView.appendChild(linkView)
+
+        linha.appendChild(colunaNome)
+        linha.appendChild(colunaView)
+        
+        document.querySelector('table#graficos tbody').appendChild(linha)
     }catch(e)
     {
         console.log(e)
     }
     
+}
+
+function createPie() {
+    const mortes = document.querySelector('input#chkMortes').checked
+    const sltCSV = document.querySelector('select#csv')
+    const csv = sltCSV.options[sltCSV.selectedIndex].value
+    let cobertura = ''
+    if(csv == 'States') {
+        cobertura = 'city'
+    }else {
+        cobertura = csv
+    }
+
+    const obj = {mortes: {Value: mortes}}
+    if(cobertura === 'city') {
+        const sltValor = document.querySelector('select#dados')
+        const valor = sltValor.options[sltValor.selectedIndex].value
+        obj.valor = {Value: valor}
+    }
+
+    const url = FLASK_API_BASE_URL+`pie/${cobertura}`
+
+    plotsAjax(url, obj, 'Pizza')
+    /*console.log(JSON.stringify(obj))
+    const resposta = await axios.post("https://covid-19-flask-api.herokuapp.com/teste", JSON.stringify(obj))
+    console.log(resposta)*/
+}
+
+function createTotalBar() {
+    const mortes = document.querySelector('input#chkMortes').checked
+    const sltCSV = document.querySelector('select#csv')
+    const csv = sltCSV.options[sltCSV.selectedIndex].value
+
+    const sltRatio = document.querySelector('select#sltRatio')
+    const taxa = sltRatio.options[sltRatio.selectedIndex].value
+
+    
+    const obj = {mortes: {Value: mortes}, taxa: {Value: taxa}}
+
+    let complemento = 'totalbar/'
+    if(csv == 'region') {
+        complemento += 'state'
+    }else if(csv == 'States') {
+        const sltValor = document.querySelector('select#dados')
+        const valor = sltValor.options[sltValor.selectedIndex].value
+        obj.estado = {Value: valor}
+        complemento += 'city'
+    }
+
+    const url = FLASK_API_BASE_URL+complemento
+    plotsAjax(url, obj, 'Barra')
 }
 
 /*
@@ -64,7 +145,9 @@ async function ajaxGraficoEstatico() {
     console.log(resposta)
 }
 */
-function construirGraficoDinamico() {
+function createDinamicPlots() {
+    const radios = Array.from(document.querySelectorAll('input[name=grafico]'))
+    const radioEscolhido = radios.find(radio => radio.checked)
     const combo = document.querySelector('select#csv')
     const item = combo.options[combo.selectedIndex].value
     const checkboxes = Array.from(document.querySelectorAll('input[name=chk'+item+']'))
@@ -79,17 +162,28 @@ function construirGraficoDinamico() {
         mortes: {Value: mortes}
     }
 
-    let complemento = `comparison/`
-    if(corpo.selecionado.Value.length == 2)
-        complemento += `Two`
-    else
-        complemento += `Multiple`
+    let complemento = `/${radioEscolhido.value}/`
 
-    ajaxGraficoDinamico(FLASK_API_BASE_URL+complemento,corpo)
+    if(item == 'Cities')
+        complemento += 'cities'
+    else
+        complemento += 'states'
+    
+    let tipo = ''
+    if(radioEscolhido.value == 'comparison') {
+        tipo = 'Comparação'
+        if(corpo.selecionado.Value.length == 2)
+            complemento += `/Two`
+        else
+            complemento += `/Multiple`
+    }else {
+        tipo = 'Mapa de Calor'
+    }
+    plotsAjax(FLASK_API_BASE_URL+complemento,corpo, tipo)
 }
 
 
-async function navegarAjax(link, destino, push = true) {
+async function ajaxNavigation(link, destino, push = true) {
     if(!link || !destino) return
     const div = document.querySelector(destino)
     try {
