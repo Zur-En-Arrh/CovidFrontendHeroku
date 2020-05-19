b45c22a8a19169791c619531738d93b1ef33042b
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-18 22:25:06 -0300

    *Mods-Vinicius*

--- js/funcoes-graficos.js
+++ js/funcoes-graficos.js
@@ -1,22 +1,58 @@
 const FLASK_API_BASE_URL = 'https://covid-19-flask-api.herokuapp.com/'
-//const JS_API_BASE_URL = 'http://localhost:3000/'
-const JS_API_BASE_URL = 'https://covidcoleta.herokuapp.com/'
+const JS_API_BASE_URL = 'http://localhost:3000/'
+//const JS_API_BASE_URL = 'https://covidcoleta.herokuapp.com/'
+
+let elementos = []
+
+function choosePath()
+{
+    const combo = document.querySelector('select#csv')
+    const item = combo.options[combo.selectedIndex].value
+    if(item == 'estados')
+    {
+        elementos = []
+        if(document.querySelector('select#dados')){
+            const estadoComboBox = document.querySelector('select#dados')
+            const label = document.querySelector('label[for=dados]')
+            estadoComboBox.parentNode.removeChild(estadoComboBox)
+            label.parentNode.removeChild(label)
+        }
+        document.querySelector('ul#lista').innerHTML = ''
+        document.querySelector('div#chkDados').innerHTML = ''
+        fillComboBox()
+        
+    }else{
+        elementos = []
+        document.querySelector('ul#lista').innerHTML = ''
+        if(document.querySelector('div#chkDados').innerHTML !== ''){
+            const estadoComboBox = document.querySelector('select#dados')
+            const label = document.querySelector('label[for=dados]')
+            estadoComboBox.parentNode.removeChild(estadoComboBox)
+            label.parentNode.removeChild(label)
+        }
+        createCheckBoxes('cidade')
+    }
+}
+
 function fillComboBox() {
     const formGroup = document.createElement('div')
     formGroup.setAttribute('class', 'form-group')
+
     const label = document.createElement('label')
     label.setAttribute('for', 'dados')
-    const combo = document.querySelector('select#csv')
+
     const elemento = document.createElement('select')
     elemento.setAttribute('id', 'dados')
     elemento.setAttribute('class', 'form-control')
+    
+    const combo = document.querySelector('select#csv')
     const item = combo.options[combo.selectedIndex].value
     if(item == 'Cities')
         label.innerText = 'Cidades'
     else
         label.innerText = 'Estados'
     formGroup.appendChild(label)
-    if(item === 'Cities' || item === 'States') {
+    if(item === 'Cities' || item === 'estados') {
         axios(JS_API_BASE_URL+`data/${item}`)
             .then(resposta => {
                 elemento.innerHTML = ''
@@ -25,25 +61,42 @@ function fillComboBox() {
                     child.innerHTML = obj
                     elemento.appendChild(child)
                 })
+                if(document.querySelector('input[type=radio]#radioHeatmap'))
+                    elemento.setAttribute('onchange', "createCheckBoxes('estado')")
                 formGroup.appendChild(elemento)
                 document.querySelector('div#select-group').appendChild(formGroup)
             })
             .catch(err => console.log(err))
+    }else {
+        if(document.querySelector('select#dados')) {
+            const estadoComboBox = document.querySelector('select#dados')
+            const label = document.querySelector('label[for=dados]')
+            estadoComboBox.parentNode.removeChild(estadoComboBox)
+            label.parentNode.removeChild(label)
+        }
     }
 }
 
-function createCheckBoxes() {
-
-    const combo = document.querySelector('select#csv')
+function createCheckBoxes(type) {
+    let url = JS_API_BASE_URL
+    if(type == 'cidade')
+    {
+        url += `data/estados`
+    }else if (type == 'estado')
+    {
+        const combo = document.querySelector('select#dados')
+        const item = combo.options[combo.selectedIndex].value
+        url += `data/cidades/${item}`
+    }
     const elemento = document.querySelector('div#chkDados')
-    const item = combo.options[combo.selectedIndex].value
-    axios(JS_API_BASE_URL+`data/${item}`)
+    axios(url)
         .then(resposta => {
             elemento.innerHTML = ''
-            resposta.data.forEach(obj => {
+            resposta.data.forEach(cidade => {
+                const findCity = obj => obj === cidade
                 const child = `<div class="form-check"> 
-                <input type="checkbox" class="form-check-input" id="id${obj}" value="${obj}" name="chk${item}"/> 
-                <label class="form-check-label" for="id${obj}">${obj}</label>
+                <input type="checkbox" onclick="updateList('${cidade}')" class="form-check-input" id="id${cidade}" value="${cidade}" name="chk${type}" ${elementos.find(findCity)?'checked':''}/> 
+                <label class="form-check-label" for="id${cidade}">${cidade}</label>
                 </div>`
                 elemento.innerHTML += child
             })
@@ -51,6 +104,24 @@ function createCheckBoxes() {
         .catch(err => console.log(err))
 }
 
+
+function updateList(cidade) {
+    const findCity = obj => obj === cidade
+    const lista = document.querySelector('ul#lista')
+    const id = `li${cidade.split(' ').join('')}`
+    if(!elementos.find(findCity)) {
+        const itemLista = document.createElement('li')
+        itemLista.setAttribute('id', id)
+        itemLista.innerText = cidade
+        lista.appendChild(itemLista)
+        elementos.push(cidade)
+    }else {
+        const deletedItem = document.querySelector('li#'+id)
+        deletedItem.parentNode.removeChild(deletedItem)
+        delete elementos[elementos.indexOf(cidade)]
+    }
+    console.log(elementos)
+}
 
 async function plotsAjax(url,body, tipo) {
     //console.log(JSON.stringify(body))
@@ -85,8 +156,10 @@ function createPie() {
     const sltCSV = document.querySelector('select#csv')
     const csv = sltCSV.options[sltCSV.selectedIndex].value
     let cobertura = ''
-    if(csv == 'States') {
+    if(csv == 'states') {
         cobertura = 'city'
+    }else if(csv == 'estados') {
+        cobertura = 'state'
     }else {
         cobertura = csv
     }
@@ -120,7 +193,7 @@ function createTotalBar() {
     let complemento = 'totalbar/'
     if(csv == 'region') {
         complemento += 'state'
-    }else if(csv == 'States') {
+    }else if(csv == 'estados') {
         const sltValor = document.querySelector('select#dados')
         const valor = sltValor.options[sltValor.selectedIndex].value
         obj.estado = {Value: valor}
@@ -149,22 +222,28 @@ function createDinamicPlots() {
     const radios = Array.from(document.querySelectorAll('input[name=grafico]'))
     const radioEscolhido = radios.find(radio => radio.checked)
     const combo = document.querySelector('select#csv')
-    const item = combo.options[combo.selectedIndex].value
-    const checkboxes = Array.from(document.querySelectorAll('input[name=chk'+item+']'))
+    const item = combo.options[combo.selectedIndex].text
+    //const chkId = item == 'Por Cidades'?'City':'State'
+    /*
+    const checkboxes = Array.from(document.querySelectorAll('input[name=chk'+chkId+']'))
     const selecionados = chk => chk.checked
     const valor = chk => chk.value
+    */
+
     const mortes = document.querySelector('input#chkMortes').checked
 
+    const filtro = elementos.filter(obj => obj !== undefined)
+
     const corpo = {
-        parametro: {Value: item},
-        tipo: {Value: 'bar'},
-        selecionado: {Value: checkboxes.filter(selecionados).map(valor)},
+        selecionado: {Value: filtro},
         mortes: {Value: mortes}
     }
 
-    let complemento = `/${radioEscolhido.value}/`
+    console.log(JSON.stringify(corpo))
 
-    if(item == 'Cities')
+    let complemento = `${radioEscolhido.value}/`
+
+    if(item == 'Por Cidades')
         complemento += 'cities'
     else
         complemento += 'states'
